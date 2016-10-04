14617a6c78092dc98643e4d60b5dc49247efef9f
Nina Tuluptseva <p.pin.p@yandex.ru>
2016-10-04 18:23:48 +0300

    First step

---
+++ InputFormat/src/Test.java
@@ -0,0 +1,18 @@
+/**
+ * Created by nina on 03.10.16.
+ */
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+
+import java.util.zip.Inflater;
+
+public class Test {
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+
+
+        Inflater decompressor = new Inflater();
+        decompressor.
+    }
+}
---
+++ InputFormat/src/com/nina/technosphere/hadoop/MyFileInputFormat.java
@@ -0,0 +1,10 @@
+package com.nina.technosphere.hadoop;
+
+/**
+ * Created by nina on 04.10.16.
+ */
+
+
+public class MyFileInputFormat {
+
+}
